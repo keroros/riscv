e5b9fa6387dbf79b6f2854c603a0aab12528a949
keroros <2862478683@qq.com>
2024-11-08 12:45:26 +0800

    update

--- core/cache/dcache/QA_DIR/verdiLog/verdi.cmd
+++ core/cache/dcache/QA_DIR/verdiLog/verdi.cmd
@@ -8,11 +8,23 @@ debLoadSimResult \
 wvCreateWindow
 wvRestoreSignal -win $_nWave2 "signal.rc" -overWriteAutoAlias on -appendSignals \
            on
-verdiSetFont -monoFont "Courier" -monoFontSize "12"
-simSetSvtbMode off
-verdiSetFont -monoFont "Courier" -monoFontSize "24"
-verdiSetFont -monoFont "Courier" -monoFontSize "18"
-verdiSetFont -monoFont "Courier" -monoFontSize "14"
 verdiSetFont -monoFont "Fixed" -monoFontSize "20"
+simSetSvtbMode off
+verdiSetFont -monoFont "Fixed" -monoFontSize "14"
+verdiSetFont -monoFont "Fixed" -monoFontSize "20"
+srcDeselectAll -win $_nTrace1
+srcHBSelect "dcache_tb.u_dcache" -win $_nTrace1
+srcHBSelect "dcache_tb.u_dcache" -win $_nTrace1
+srcSetScope -win $_nTrace1 "dcache_tb.u_dcache" -delim "."
+srcHBSelect "dcache_tb.u_dcache" -win $_nTrace1
+srcDeselectAll -win $_nTrace1
+srcDeselectAll -win $_nTrace1
+verdiSetFont -monoFont "Fixed" -monoFontSize "14"
+verdiSetFont -monoFont "Fixed" -monoFontSize "20"
+verdiDockWidgetMaximize -dock windowDock_nWave_2
+verdiDockWidgetRestore -dock windowDock_nWave_2
+srcDeselectAll -win $_nTrace1
+srcSelect -signal "MAIN_REFILL" -line 169 -pos 1 -win $_nTrace1
+srcDeselectAll -win $_nTrace1
 srcDeselectAll -win $_nTrace1
 debExit
